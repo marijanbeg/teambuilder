13accf8eaad59a7b89d61f8081712d9d47f07d99
marijanbeg <m.beg@imperial.ac.uk>
2023-11-17 12:56:34 +0000

    File for generating test set.

---
+++ teambuilder/tests/data/create.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import phantombunch as pb
+
+n = 250  # number of students in the cohort
+
+# Create cohort.
+while not pb.valid_cohort(cohort := pb.cohort(n)):
+    cohort = pb.cohort(250)
+
+# Create marks.
+marks = pb.marks(
+    usernames=cohort.usernames, mean=65, sd=6, fail_probability=0.02, feedback=False
+)
+
+# Add marks to cohort.
+cohort = pd.merge(cohort, marks, on="username")
+
+# Merge first and last names.
+cohort["name"] = cohort.first_name + " " + cohort.last_name
+
+# Keep only relevant columns.
+cohort = cohort[
+    [
+        "username",
+        "name",
+        "github",
+        "course",
+        "gender",
+        "mark",
+    ]
+]
+
+cohort.to_csv("cohort.csv", index=False)
